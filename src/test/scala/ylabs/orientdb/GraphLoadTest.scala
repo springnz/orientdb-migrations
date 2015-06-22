bb12ddc173038a470c57504037a3246f21c21c82
Michael Pollmeier <michael@michaelpollmeier.com>
2015-06-22 13:54:22 +1200

    add graph load test

---
+++ src/test/scala/ylabs/orientdb/GraphLoadTest.scala
@@ -0,0 +1,117 @@
+package ylabs.orientdb
+
+import com.orientechnologies.orient.core.intent.OIntentMassiveInsert
+import org.scalatest.WordSpec
+import org.scalatest.ShouldMatchers
+import collection.JavaConversions._
+import concurrent.ExecutionContext.Implicits.global
+import collection.mutable
+import com.tinkerpop.blueprints.impls.orient._
+import com.tinkerpop.blueprints.TransactionalGraph.Conclusion
+
+class GraphLoadTest extends WordSpec with ShouldMatchers {
+  // import OrientDBScala._
+
+  // first need to run the following with console.sh:
+  // CREATE DATABASE remote:localhost/graphtest root root plocal graph
+
+  // val graphFactory = new OrientGraphFactory("plocal:target/databases/test" + math.random)
+  // val graphFactory = new OrientGraphFactory("memory:test")
+  val graphFactory = new OrientGraphFactory("remote:localhost/graphtest")
+  val graph = graphFactory.setupPool(1, 10)
+    // .getTx
+  .getNoTx
+
+  object Labels extends Enumeration {
+    val Listing, User, Session, HadSession, ViewListing, ViewNumber = Value
+  }
+  import Labels._
+  type Id = String
+
+  val ids = mutable.Map.empty[Labels.Value, Seq[Id]].withDefaultValue(Seq.empty)
+
+  def createVertices(count: Int, label: Labels.Value)(properties: => Map[String, AnyRef]): Unit = {
+    (1 to count) foreach { i ⇒
+      val props = properties + ("lbl" -> label.toString)
+      // blueprints api expect tuples of key/value in one list...
+      val propsList = properties.map { case (key, value) => Seq(key, value) }.flatten.toSeq
+      val v = graph.addVertex(null, propsList: _*)
+      ids.update(label, ids(label) :+ v.getId.toString)
+      // if (i % 1000 == 0) graph.stopTransaction(Conclusion.SUCCESS)
+    }
+  }
+
+  def randomVertex(label: Labels.Value) = {
+    val list = ids(label)
+    val number = math.random * list.size
+    graph.getVertex(list(number.toInt))
+  }
+
+  def addSessions(count: Int) = 
+    (1 to count) foreach { _ ⇒
+      val user = randomVertex(User)
+      val session = graph.addVertex(null, "lbl", Session.toString)
+      ids.update(Session, ids(Session) :+ session.getId.toString)
+
+      graph.addEdge(null, user, session, HadSession.toString)
+      //TODO: add a random property to this, e.g. the time
+    }
+
+  def createListingViews(count: Int) = 
+    (1 to count) foreach { _ ⇒
+      val session = randomVertex(Session)
+      val listing = randomVertex(Listing)
+      graph.addEdge(null, session, listing, ViewListing.toString)
+      //TODO: add a random property to this, e.g. the time
+    }
+
+  def createViewNumberEvents(count: Int) = 
+    (1 to count) foreach { _ ⇒
+      val session = randomVertex(Session)
+      val listing = randomVertex(Listing)
+      graph.addEdge(null, session, listing, ViewNumber.toString)
+      //TODO: add a random property to this, e.g. the time
+    }
+
+  "tinkerpop api" should {
+    "create scenario graph" in {
+      println("starting to delete the existing elements")
+      graph.getEdges.foreach(_.remove())
+      graph.getVertices.foreach(_.remove())
+      println("deleted all elements")
+
+      val userCount = 10
+      time {
+        createVertices(userCount, User){ Map("url" -> s"http://us.co.nz/${math.random}") }
+        // createVertices(userCount, User)
+        // addSessions(userCount * 3)
+        // createVertices(20, Listing)
+        // createListingViews(userCount * 6)
+        // createViewNumberEvents(userCount * 2)
+      }
+
+      println("vertex count: " + graph.getVertices.size)
+      println("edge count: " + graph.getEdges.size)
+    }
+
+    // "insert some DB records" ignore {
+    //   time {
+    //     val luca = graph.addVertex(null, "name", "Luca")
+    //     val marko = graph.addVertex(null, "name", "Marko")
+    //     val lucaKnowsMarko = graph.addEdge(null, luca, marko, "knows")
+
+    //     graph.getVertices.asScala.foreach(vertex ⇒ println(vertex.getProperty("name")))
+    //   }
+    // }
+  }
+
+  def numberBetween(lower: Int, upper: Int): Int = (math.random * upper - lower).toInt
+
+  def time[R](block: ⇒ R): R = {
+    val t0 = System.nanoTime()
+    val result = block // call-by-name
+    val t1 = System.nanoTime()
+    println("Elapsed time: " + (t1 - t0) / 1000000 + "ms")
+    result
+  }
+}
