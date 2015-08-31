099631b7fce4726ba433d7e36030f1f79f7cd855
Mike Ourednik <mike@spring.co.nz>
2015-08-31 13:18:43 +1200

    cleanups

--- src/test/scala/ylabs/orientdb/GraphDBTest.scala
+++ src/test/scala/ylabs/orientdb/GraphDBTest.scala
@@ -2,7 +2,7 @@ package ylabs.orientdb
 
 import com.tinkerpop.blueprints.impls.orient._
 import org.scalatest.{ ShouldMatchers, WordSpec }
-import ylabs.orientdb.test.ODBRemoteTestTag
+import ylabs.orientdb.test.ODBMemoryTestTag
 
 import scala.collection.JavaConverters._
 import scala.collection.mutable
@@ -26,6 +26,7 @@ class GraphDBTest extends WordSpec with ShouldMatchers {
   val ids = mutable.Map.empty[Labels.Value, Seq[Id]].withDefaultValue(Seq.empty)
 
   def createVertices(count: Int, label: Labels.Value): Unit = {
+    println(s"creating $count vertices")
     (1 to count) foreach { i ⇒
       val v = graph.addVertex(null, "lbl", label.toString)
       ids.update(label, ids(label) :+ v.getId.toString)
@@ -39,7 +40,8 @@ class GraphDBTest extends WordSpec with ShouldMatchers {
     graph.getVertex(list(number.toInt))
   }
 
-  def addListingViews(count: Int) =
+  def addListingViews(count: Int) = {
+    println(s"creating $count edges")
     (1 to count) foreach { _ ⇒
       val user = randomVertex(User)
       val listing = randomVertex(Listing)
@@ -47,43 +49,13 @@ class GraphDBTest extends WordSpec with ShouldMatchers {
       //TODO: add a random property to this, e.g. the time
       // user.addEdge(ViewListing.toString, listing, Map(s"random property ${math.random}" → math.random))
     }
+  }
 
-  "tinkerpop api" should {
-
-    "create scenario graph" taggedAs ODBRemoteTestTag in {
-      println("starting to delete the existing elements")
-      graph.getEdges.asScala.foreach(_.remove())
-      graph.getVertices.asScala.foreach(_.remove())
-      println("deleted all elements")
-      // time {
-      //   (1 to 1000000) foreach { i ⇒
-      //     val v = graph.addVertex(null, "name", s"name $i")
-      //     // println(v.getBaseClassName)
-
-      //     if (i % 1000 == 0) graph.stopTransaction(Conclusion.SUCCESS)
-      //   }
-      //   // graph.declareIntent(null)
-      // }
-
-      time {
-        createVertices(100, Listing)
-        createVertices(100, User)
-        addListingViews(200)
-      }
-
-      println("vertex count: " + graph.getVertices.asScala.size)
-      println("edge count: " + graph.getEdges.asScala.size)
-    }
-
-    "insert some DB records" ignore {
-      time {
-        val luca = graph.addVertex(null, "name", "Luca")
-        val marko = graph.addVertex(null, "name", "Marko")
-        val lucaKnowsMarko = graph.addEdge(null, luca, marko, "knows")
-
-        graph.getVertices.asScala.foreach(vertex ⇒ println(vertex.getProperty("name")))
-      }
-    }
+  def deleteAllElements(graph: OrientGraphNoTx): Unit = {
+    println("starting to delete the existing elements")
+    graph.getEdges.asScala.foreach(_.remove())
+    graph.getVertices.asScala.foreach(_.remove())
+    println("deleted all elements")
   }
 
   def time[R](block: ⇒ R): R = {
@@ -92,5 +64,32 @@ class GraphDBTest extends WordSpec with ShouldMatchers {
     val t1 = System.nanoTime()
     println("Elapsed time: " + (t1 - t0) / 1000000 + "ms")
     result
+  }
+
+  "tinkerpop api" should {
+
+    "create scenario graph" taggedAs ODBMemoryTestTag in {
+      deleteAllElements(graph)
+      time {
+        createVertices(100, Listing)
+        createVertices(100, User)
+        addListingViews(200)
+      }
+      graph.getVertices.asScala.size shouldBe 200
+      graph.getEdges.asScala.size shouldBe 200
+    }
+
+    "insert some DB records" taggedAs ODBMemoryTestTag in {
+      time {
+        val luca = graph.addVertex(null, "name", "Luca")
+        val marko = graph.addVertex(null, "name", "Marko")
+        val lucaKnowsMarko = graph.addEdge(null, luca, marko, "knows")
+
+        val names = graph.getVertices.asScala
+          .flatMap(vertex ⇒ Option(vertex.getProperty[String]("name")))
+          .toSet
+        names shouldBe Set("Luca", "Marko")
+      }
+    }
   }
 }
