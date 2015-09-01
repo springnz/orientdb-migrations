46ee5b77aa85f654f1d37c9af79af412b0c7981c
Mike Ourednik <mike@spring.co.nz>
2015-09-01 15:18:40 +1200

    Close graph after session

--- src/main/scala/ylabs/orientdb/session/ODBGraphSession.scala
+++ src/main/scala/ylabs/orientdb/session/ODBGraphSession.scala
@@ -5,8 +5,11 @@ import com.tinkerpop.blueprints.impls.orient.OrientGraph
 final case class ODBGraphSession[+A](override val block: OrientGraph ⇒ A)
     extends AbstractODBSession[A, OrientGraph](block) {
 
-  def run(graph: OrientGraph): A =
-    block(graph)
+  def run(graph: OrientGraph): A = {
+    val result = block(graph)
+    close(graph)
+    result
+  }
 
   def close(graph: OrientGraph): Unit =
     graph.shutdown()
