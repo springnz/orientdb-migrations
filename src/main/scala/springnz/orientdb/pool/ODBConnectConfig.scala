6247e243e21f8e8a517844a9808f41007b4a5999
Mike Ourednik <mike@spring.co.nz>
2015-10-09 15:38:43 +1300

    Deleted unused class

--- src/main/scala/springnz/orientdb/pool/ODBConnectConfig.scala
+++ src/main/scala/springnz/orientdb/pool/ODBConnectConfig.scala
@@ -5,7 +5,4 @@ case class ODBConnectConfig(
   override def toString = s"ODBConnectConfig(url=$url, user=$user)"
 }
 
-case class ODBTP2ConnectConfig(
-  url: String, user: String, pass: String, minPoolSize: Int, maxPoolSize: Int) {
-  override def toString = s"ODBGraphTP2ConnectConfig(url=$url, user=$user, minPoolSize=$minPoolSize, maxPoolSize=$maxPoolSize)"
-}
+
